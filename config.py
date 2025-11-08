f057ede17556ccb0903efd17fbacf0a99bc16ba3
zpvlc <sexpaveldurov@outlook.com>
2025-11-08 15:40:47 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-YANDEX_TOKEN = os.getenv("YANDEX_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
